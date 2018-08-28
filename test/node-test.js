05db809380e190b543e4a269468516c8b5f6ff77
Chris Chateau <christopherchateau@gmail.com>
2018-08-28 15:46:14 -0600

    Create insert method

--- test/node-test.js
+++ test/node-test.js
@@ -7,21 +7,17 @@ import Node from '../lib/node';
 
   beforeEach( () => {
     node = new Node('p');
-  })
+  });
 
   it('should exist right now', () => {
     expect(node).to.exist;
-  })
+  });
 
   it('should default children to be an empty obj', () => {
     expect(node.children).deep.equal({});
-  })
-
-  it('should take a letter as an arg and assign it to the letter property', () => {
-    expect(node.letter).to.equal('p');
-  })
+  });
 
   it('should default end to false', () => {
     expect(node.end).to.equal(false)
-  })
+  });
 });
--- test/trie-test.js
+++ test/trie-test.js
@@ -9,20 +9,30 @@ describe('TRIE', () => {
     prefixTrie = new Trie;
   });
 
-  it('should exist right now', () => {
+  it('should exist', () => {
     expect(prefixTrie).to.exist;
-  })
+  });
 
   it('should have a default length on zero', () => {
     expect(prefixTrie.wordCount).to.equal(0);
-  })
+  });
+
   it('should have a default root of null', () => {
-    expect(prefixTrie.root).to.equal(null);
-  })
+    expect(prefixTrie.root.children).to.deep.equal({});
+  });
+
   it('should increase word-count when a new word is added', () => {
     expect(prefixTrie.count()).to.equal(0);
     prefixTrie.insert('pizza');
     expect(prefixTrie.count()).to.equal(1);
-  })
+  });
+
+  it('should insert word correctly', () => {
+    prefixTrie.insert('pizza');
+    prefixTrie.insert('race');
+    prefixTrie.insert('racecar');
+    console.log(JSON.stringify(prefixTrie, null, 4));
+    // expect(prefixTrie.root.children.letter).to.equal('p');
+  });
 
 });
